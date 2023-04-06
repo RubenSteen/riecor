0e816dfd7d2d92637a039f67ed3e3f2ee89cfd76
Ruben Steen <rubensteen123@gmail.com>
2023-04-06 14:30:13 +0000

    Added ArchTests

---
+++ tests/Feature/ArchTest.php
@@ -0,0 +1,9 @@
+<?php
+
+it('does not use debugging functions')
+    ->expect(['dd', 'dump', 'ray'])
+    ->not->toBeUsed();
+
+it('uses the redirect facade for redirecting')
+    ->expect(['back', 'redirect', 'to_route'])
+    ->not->toBeUsedIn("App\\Http\\Controllers\\");
