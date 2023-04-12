0fd8e220130e5e1d76015a4a4d856b0511614d42
Ruben Steen <rubensteen123@gmail.com>
2023-04-12 11:03:38 +0000

    Added 2 others tests

--- tests/Feature/Models/FollowTest.php
+++ tests/Feature/Models/FollowTest.php
@@ -23,3 +23,33 @@ it('can retrieve a list of the user their followers', function () {
 
     expect($user->followers)->toHaveCount(3);
 });
+
+it('can retrieve the first user it follows', function () {
+    $user = User::factory()->create();
+
+    $usersToFollow = User::factory()->count(3)->create();
+
+    $user->follows()->attach($usersToFollow);
+
+    expect($user->follows->first()->email)->toBe($usersToFollow->first()->email);
+});
+
+it('can retrieve the created_at from the pivot table', function () {
+    $user = User::factory()->create();
+
+    $usersToFollow = User::factory()->count(3)->create();
+
+    $user->follows()->attach($usersToFollow);
+
+    expect($user->follows()->first()->pivot->created_at)->toBeInstanceOf(Illuminate\Support\Carbon::class);
+});
+
+it('can retrieve the updated_at from the pivot table', function () {
+    $user = User::factory()->create();
+
+    $usersToFollow = User::factory()->count(3)->create();
+
+    $user->follows()->attach($usersToFollow);
+
+    expect($user->follows()->first()->pivot->updated_at)->toBeInstanceOf(Illuminate\Support\Carbon::class);
+});
